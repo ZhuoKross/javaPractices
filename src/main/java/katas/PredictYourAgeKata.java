f402c143e9fef8c6e375b89df555300f1b5a9cca
Michael Garnica <mgarnica.oficial@gmail.com>
2025-07-09 11:44:23 -0500

    first charge

---
+++ src/main/java/katas/PredictYourAgeKata.java
@@ -0,0 +1,18 @@
+package katas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PredictYourAgeKata {
+    public Integer predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8){
+
+        List<Integer> listOfAges = List.of(age1, age2, age3, age4, age5, age6, age6, age7, age8);
+
+
+
+
+
+
+        return null;
+    }
+}
