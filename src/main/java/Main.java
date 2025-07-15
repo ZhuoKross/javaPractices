7878f27bf3d6760dba911f07eda457b9aeccce96
Michael Garnica <mgarnica.oficial@gmail.com>
2025-07-15 10:05:34 -0500

    making the first kata

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import katas.PredictYourAgeKata;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("it works");
+
+        PredictYourAgeKata predictYourAge = new PredictYourAgeKata();
+
+        System.out.println(predictYourAge.predictAge(65,60,75,55,60,63,64,45));
     }
 }
--- src/main/java/katas/PredictYourAgeKata.java
+++ src/main/java/katas/PredictYourAgeKata.java
@@ -1,18 +1,23 @@
 package katas;
 
+import com.sun.tools.jconsole.JConsoleContext;
+
+import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class PredictYourAgeKata {
-    public Integer predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8){
+    public int predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8){
 
-        List<Integer> listOfAges = List.of(age1, age2, age3, age4, age5, age6, age6, age7, age8);
+        List<Integer> listOfAges = List.of(age1, age2, age3, age4, age5, age6, age7, age8);
 
+        var sumAges = listOfAges.stream()
+                .mapToInt(age ->
+                        age * age)
+                .sum();
 
-
-
-
-
-        return null;
+        return (int) Math.sqrt(sumAges) / 2;
     }
 }
